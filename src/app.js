062373dd3208a17983f34c6884543c230363a629
Rafael Jiménez Cabrera <33703980+Raejimenezca@users.noreply.github.com>
2019-03-30 08:37:05 -0500

    'Registro' e 'ingreso' implementados.
     para discriminar los usuarios 'coordinadores' y 'aspirantes'.

--- src/app.js
+++ src/app.js
@@ -27,15 +27,47 @@ app.get('/ingreso', (req, res) => {
     res.render('ingreso');
 });
 
-app.get('/cursos_disponibles', (req, res) => {
+app.get('/administracion', (req, res) => {
     console.log(req.query);
-    crud.crear(req.query);
-    res.render('cursos_disponibles', {
-        nombre: req.query.nombre,
-        id: req.query.id,
-        correo: req.query.correo,
-        telefono: req.query.telefono
-    });
+    crud.listar();
+    let estudiante = listaEstudiantes.find(busqueda => busqueda.id == req.query.id);
+    if (!estudiante) {
+        console.log('Los datos ingresados no son validos');
+        res.render('ingreso');
+    }
+    else {
+        if (estudiante.tipo == 'coordinador') {
+            res.render('administracion', {
+                nombre: estudiante.nombre,
+                id: parseInt(estudiante.id),
+                correo: estudiante.correo,
+                telefono: estudiante.telefono
+            });
+        }
+        else {
+            console.log('Los datos ingresados no son validos');
+            res.render('ingreso');
+        }
+    }
+});
+
+app.get('/cursos_disponibles', (req, res) => {
+    crud.listar();
+    let estudiante = listaEstudiantes.find(buscar => buscar.id == req.query.id);
+    
+    if (!estudiante) {
+        crud.crear(req.query);
+        res.render('cursos_disponibles', {
+            nombre: req.query.nombre,
+            id: parseInt(req.query.id),
+            correo: req.query.correo,
+            telefono: req.query.telefono
+        });
+    }
+    else {
+        console.log('Registo invalido');
+        res.render('registro');
+    }
 });
 
 app.get('*', (req, res) => {
--- src/crud.js
+++ src/crud.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 listaEstudiantes = [];
 
 const crear = (estudiante) => {
+    listar();
     let est = {
         id: estudiante.id,
         nombre: estudiante.nombre,
@@ -12,13 +13,35 @@ const crear = (estudiante) => {
     let duplicado = listaEstudiantes.find(nom => nom.id == estudiante.id)
     if (!duplicado){
         listaEstudiantes.push(est);
-        console.log(listaEstudiantes);
+        //console.log(listaEstudiantes);
+        guardar();
     }
     else {
         console.log('Ya existe un estudiante con ese ID')
     }
 }
 
+const guardar = () => {
+    //console.log(listaEstudiantes);
+    let datos = JSON.stringify(listaEstudiantes);
+    fs.writeFile('database.json', datos, (err) => {
+        if (err) throw (err);
+        console.log('Archivo creado con éxito');
+    })
+}
+
+const listar = () => {
+    try {
+        listaEstudiantes = require('../database.json');
+    }
+    catch (error) {
+        listaEstudiantes = [];
+    }
+    //listaEstudiantes = JSON.parse(fs.readFileSync('listado.json'));
+}
+
 module.exports = {
-    crear
+    crear,
+    guardar,
+    listar
 }
